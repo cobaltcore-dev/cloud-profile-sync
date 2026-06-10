26aff9ca6a90d1c692ac79da938f5dbf5c9939d5
Aliaksei Dziauho <a.dziauho@sap.com>
2026-06-10 13:31:00 +0200

    fix: gvk in tests

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -9,6 +9,7 @@ package v1alpha1
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
@@ -16,7 +17,10 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "cloudprofilesync.cobaltcore.dev", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = runtime.NewSchemeBuilder()
+	SchemeBuilder = runtime.NewSchemeBuilder(func(s *runtime.Scheme) error {
+		metav1.AddToGroupVersion(s, GroupVersion)
+		return nil
+	})
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
